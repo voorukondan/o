f72bcea144a03ded4377dbbe923ad7096bb9ccbc
voorukondan <voorukondan@gmail.com>
2024-01-28 15:23:59 +0000

    solution

---
+++ src/main/java/com/example/song/repository/SongJpaRepository.java
@@ -0,0 +1,10 @@
+package com.example.song.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.song.model.Song;
+
+@Repository
+public interface SongJpaRepository extends JpaRepository<Song, Integer> {
+
+}
